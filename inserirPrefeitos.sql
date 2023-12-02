caf4ed603ac7fa27e6b8c37713e9b8da2fb4440f
thalisson_danelon <thalisson.danelon@digifred.com.br>
2023-12-02 17:23:50 -0300

    update

---
+++ inserirPrefeitos.sql
@@ -0,0 +1,14 @@
+SELECT * FROM prefeitos;
+
+
+INSERT INTO prefeitos
+(nome, cidade_id)
+VALUES
+    ('Rodrigo Neves', 3),
+    ('Raquel Lyra', 4),
+    ('Zenaldo Coutinho', null);
+
+insert into prefeitos
+    (nome, cidade_id)
+values
+    ('Rafael Greca', null);
